d9a07fb4c992c8380380c20dad3a958e0576bc99
OrenTK <schoro@post.bgu.ac.il>
2024-03-08 16:21:14 +0200

    update after friday meeting

--- Auxiliary Functions/project_meshrefine.m
+++ Auxiliary Functions/project_meshrefine.m
@@ -19,9 +19,7 @@ function [mesh_refined, nodeIndicesWithinRadius] = project_meshrefine(mesh,centr
 %           forward mesh and 1 (mm^3) for reconstruction mesh.
 %
 % output:
-%    mesh_refined: a struct with two fields containing the refined "node" (in length unit) and "elem" lists
-%                  if the optional field "value" is defined in the input,
-%                  then mesh_refined will also have the cooresponding field "value".                      
+%    mesh_refined: a struct with two fields containing the refined "node" (in length unit) and "elem" lists                      
 %
 % dependency:
 %    this function requires "meshrefine" and "meshcentroid" from iso2mesh toolbox.
@@ -47,7 +45,7 @@ end
 % set the desired maximum volume to a maxvol for those elements
 % Calculate the centroid of each element
 
-c0 = meshcentroid(mesh.node, mesh.elem(:,1:4));
+c0 = meshcentroid(mesh.node, mesh.elem);
 
 % Calculate distance from each element's centroid to the given centroid
 dist = c0 - repmat(centroid, [size(mesh.elem,1), 1]);
@@ -61,7 +59,7 @@ sz(dist < radius) = maxvol;
 % refine the mesh
 [node_refine,elem_refine]=meshrefine(mesh.node,mesh.elem,sz);
 
-c0 = meshcentroid(node_refine, elem_refine(:,1:4));
+c0 = meshcentroid(node_refine, elem_refine);
 
 % Calculate distance from each element's centroid to the given centroid
 dist = c0 - repmat(centroid, [size(elem_refine,1), 1]);
@@ -71,23 +69,8 @@ dist = sqrt(sum(dist.*dist, 2));
 elementsWithinRadius = find(dist < radius);
 
 % Get node indices for these elements
-nodeIndicesWithinRadius = unique(elem_refine(elementsWithinRadius, 1:4));
+nodeIndicesWithinRadius = unique(elem_refine(elementsWithinRadius, :));
 
 mesh_refined.node=node_refine;
 mesh_refined.elem=elem_refine;
 
-if(isfield(mesh,'value'))
-    if(size(mesh.value,1)~=size(mesh.node,1))
-        error('the value list must match the rows of the node list');
-    end
-    isoriginal=ismember(round(mesh_refined.node*1e10)*1e-10,round(mesh.node*1e10)*1e-10,'rows');
-    newnodeidx=find(isoriginal==0); % find newly inserted nodes
-    if(length(newnodeidx))
-        [eid,weight]=tsearchn(mesh.node,mesh.elem,mesh_refined.node(newnodeidx,:));
-    end
-    mesh_refined.value=zeros(size(mesh_refined.node,1),size(mesh.value,2));
-    mesh_refined.value(find(isoriginal==1),:)=mesh.value;
-    for i=1:size(mesh.value,2)
-        mesh_refined.value(newnodeidx,i)=sum(reshape(mesh.value(mesh.elem(eid,:),i),size(weight,1),size(weight,2)).*weight,2);
-    end
-end
